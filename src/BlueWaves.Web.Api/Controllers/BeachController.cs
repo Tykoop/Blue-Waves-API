74583033a2a3917e3fbe7d82925633c3757664ae
George Leonidis <esentakos@yahoo.gr>
2020-12-28 21:51:46 +0200

    Adds AddBeach

--- src/BlueWaves.Web.Api/Controllers/BeachController.cs
+++ src/BlueWaves.Web.Api/Controllers/BeachController.cs
@@ -7,6 +7,7 @@ namespace Esentis.BlueWaves.Web.Api.Controllers
 	using System.Threading.Tasks;
 
 	using Esentis.BlueWaves.Persistence;
+	using Esentis.BlueWaves.Persistence.Helpers;
 	using Esentis.BlueWaves.Persistence.Model;
 	using Esentis.BlueWaves.Web.Api.Helpers;
 	using Esentis.BlueWaves.Web.Models;
@@ -14,6 +15,8 @@ namespace Esentis.BlueWaves.Web.Api.Controllers
 	using Microsoft.AspNetCore.Mvc;
 	using Microsoft.EntityFrameworkCore;
 	using Microsoft.Extensions.Logging;
+
+	using NetTopologySuite.Geometries;
 
 
 	[Route("api/beach")]
@@ -28,14 +31,21 @@ namespace Esentis.BlueWaves.Web.Api.Controllers
 		[HttpGet("{id}")]
 		public async Task<ActionResult<Beach>> GetBeach(long id, CancellationToken token = default)
 		{
-
 			var beach = await Context.Beaches.SingleOrDefaultAsync(x => x.Id == id, cancellationToken: token);
 			var result = new BeachDto(Id: beach.Id, Name: beach.Name, Latitude: beach.Coordinates.X,
 				Longtitude: beach.Coordinates.Y, Description: beach.Description);
 			return beach == null
 				? NotFound()
 				: Ok(result);
+		}
 
+		[HttpPost("add")]
+		public async Task<ActionResult<BeachDto>> AddBeach(AddBeachDto addBeachDto)
+		{
+			var beach = new Beach { Coordinates = new Point(addBeachDto.Latitude, addBeachDto.Longtitude), Name = addBeachDto.Name, Description = addBeachDto.Description };
+			Context.Beaches.Add(beach);
+			await Context.SaveChangesAsync();
+			return Ok(beach.toDto());
 		}
 	}
 }
--- src/BlueWaves.Web.Models/GetDto.cs
+++ src/BlueWaves.Web.Models/GetDto.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Esentis.BlueWaves.Web.Models
 {
 	public record BeachDto(long Id, string Name, double Longtitude, double Latitude, string Description);
+
+	public record AddBeachDto(string Name, double Longtitude, double Latitude, string Description);
 }
