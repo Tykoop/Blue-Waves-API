009a1e01c3ca6fa987556b19e173723b4ae0009c
George Leonidis <esentakos@yahoo.gr>
2021-01-08 22:55:25 +0200

    Adds AddRating dto

--- src/BlueWaves.Web.Models/GetDto.cs
+++ src/BlueWaves.Web.Models/GetDto.cs
@@ -3,4 +3,6 @@ namespace Esentis.BlueWaves.Web.Models
 	public record BeachDto(long Id, string Name, double Longtitude, double Latitude, string Description);
 
 	public record AddBeachDto(string Name, double Longtitude, double Latitude, string Description);
+
+	public record AddRatingDto(int Rate, long BeachId);
 }
