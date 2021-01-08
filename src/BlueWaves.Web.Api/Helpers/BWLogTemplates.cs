fa714401372218beb9411a3dfd9dbd314fc3ec22
George Leonidis <esentakos@yahoo.gr>
2021-01-08 22:55:44 +0200

    Adds new log message and edits typos

--- src/BlueWaves.Web.Api/Helpers/BWLogTemplates.cs
+++ src/BlueWaves.Web.Api/Helpers/BWLogTemplates.cs
@@ -13,9 +13,11 @@ namespace Esentis.BlueWaves.Web.Api.Helpers
 
 		public const string RequestEntities = "User requested collection of {Entity}. Found {Count} records";
 
-		public const string Conflict = "{Entity} with {Id} has assignments";
+		public const string Conflict = "{Entity} with ID {Id} has assignments";
 
-		public const string Deleted = "{Entity} with {Id} has been deleted";
+		public const string NotFound = "{Entity} not found";
+
+		public const string Deleted = "{Entity} with ID {Id} has been deleted";
 
 		public const string Updated = "{Entity} has been updated";
 	}
