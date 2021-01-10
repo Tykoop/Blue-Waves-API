dc732ae8510c7e66d4d07efb02821ae56ff2e5cb
George Leonidis <esentakos@yahoo.gr>
2021-01-10 19:45:07 +0200

    Adds migrations

--- src/BlueWaves.Persistence/Migrations/20210110174202_Adds country and continent.Designer.cs
+++ src/BlueWaves.Persistence/Migrations/20210110174202_Adds country and continent.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Esentis.BlueWaves.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -10,9 +11,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Esentis.BlueWaves.Persistence.Migrations
 {
     [DbContext(typeof(BlueWavesDbContext))]
-    partial class BlueWavesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210110174202_Adds country and continent")]
+    partial class Addscountryandcontinent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
