9265329e7a9128c7b1a33ddc2f7e6a2125d5d2c4
George Leonidis <esentakos@yahoo.gr>
2021-01-23 23:59:01 +0200

    Creates roles

---
+++ src/BlueWaves.Persistence/Helpers/RoleNames.cs
@@ -0,0 +1,13 @@
+namespace Esentis.BlueWaves.Persistence.Helpers
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public static class RoleNames
+	{
+		public const string Administrator = "ADMINISTRATOR";
+	}
+}
